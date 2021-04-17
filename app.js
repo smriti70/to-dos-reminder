0f838fa49c206683febb1cdde552f33cdbf49e4f
Smriti Singh <smriti.singh21@yahoo.com>
2021-04-17 21:43:55 +0530

    Add User-Authentication

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -13,18 +17,36 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/testingDB",{useNewUrlParser:true,useUnifiedTopology:true});
+
+mongoose.set("useCreateIndex",true);
 
 const listSchema = new mongoose.Schema({
     item: String
 });
 const userSchema = new mongoose.Schema({
     email: String,
+    password: String,
     listItems: [listSchema]
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const List = mongoose.model("List",listSchema);
 const User = mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const item1 = new List({
     item: "Welcome to your Todo list!"
@@ -47,15 +69,19 @@ app.get("/register",function(req,res){
 });
 
 app.get("/list",function(req,res){
-    List.find({},function(err,foundItems){
-        if(foundItems.length===0){
-            initials.forEach(function(todo){
-                todo.save();
-            });
-        } else {
-            res.render("list",{initialList:foundItems});
-        }
-    });
+    if(req.isAuthenticated()){
+        List.find({},function(err,foundItems){
+            if(foundItems.length===0){
+                initials.forEach(function(todo){
+                    todo.save();
+                });
+            } else {
+                res.render("list",{initialList:foundItems});
+            }
+        });
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.post("/",function(req,res){
@@ -63,14 +89,17 @@ app.post("/",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const userEmail = req.body.email;
-    const newUser = new User({
-        email: userEmail
-    });
 
-    newUser.listItems.push(item1,item2,item3);
-    newUser.save();
-    res.redirect("/list");
+    User.register({username:req.body.username}, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        } else {
+                passport.authenticate("local")(req,res,function(){
+                res.redirect("/list");
+            });
+        }
+    });
 });
 
 app.post("/list",function(req,res){
