831c9eb99b041789f2fe37b5adc7c256081b4671
Smriti Singh <smriti.singh21@yahoo.com>
2021-04-18 00:02:08 +0530

    LogIn Authentication

--- app.js
+++ app.js
@@ -85,7 +85,21 @@ app.get("/list",function(req,res){
 });
 
 app.post("/",function(req,res){
-    res.redirect("/list");
+    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            passport.authenticate('local')(req,res,function(){
+                res.redirect("/list");
+            });
+        }
+    });
 });
 
 app.post("/register",function(req,res){
